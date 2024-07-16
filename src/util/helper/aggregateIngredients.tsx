e77c974191bfa7e4a637e1e43ec0169ccaf8148b
Suzan Bulbul <suzanbulbul@Suzans-MacBook-Pro.local>
2024-07-16 10:06:48 +0300

    fix: edited aggregateIngredients helper

--- src/util/helper/aggregateIngredients.tsx
+++ src/util/helper/aggregateIngredients.tsx
@@ -2,7 +2,10 @@ import { RecipInformationType } from "../type/recipe.type";
 
 export function aggregateIngredients(data: RecipInformationType[]) {
   const allMaterials: string[] = data.reduce((acc: string[], recipe: any) => {
-    const ingredients: string[] = recipe.materials.split(", ");
+    const ingredients: string[] = recipe.materials
+      .split(/[,-]+/)
+      .map((ingredient: string) => ingredient.trim())
+      .filter((ingredient: string | any[]) => ingredient.length > 0);
 
     return acc.concat(ingredients);
   }, []);
